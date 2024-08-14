c9d11e5afff9cba01e6f93a2a1259423a55dea77
Tan Nguyen Hoang Nhat <tan.nguyenhoangnhat@bravosuite.io>
2024-08-14 18:43:54 +0700

    Done 50

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
-import { Title } from '@angular/platform-browser';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required: true}) name!: string;
   @Input({required: true}) userId!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -46,5 +47,9 @@ export class TasksComponent {
 
   completeTask(id: string){
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
