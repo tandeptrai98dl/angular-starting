5b3a0e24bd8e3bcb6d6efdc8f40c97a0e2ef04b3
Tan Nguyen Hoang Nhat <tan.nguyenhoangnhat@bravosuite.io>
2024-08-14 15:46:50 +0700

    Done 45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,19 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser()
   {
