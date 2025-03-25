b2ae1d3c9fab1ca3c6d122d98e4f9c0bcf3081fd
Tan Nguyen <tandeptrai98dl@gmail.com>
2025-03-26 01:20:24 +0700

    Done 122

--- Starting-03/src/app/dashboard/server-status/server-status.component.ts
+++ Starting-03/src/app/dashboard/server-status/server-status.component.ts
@@ -9,5 +9,22 @@ import { Component } from '@angular/core';
 })
 
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor()
+  {
+  }
+
+  ngOnInit(){
+    setInterval(() => {
+      const random = Math.random();
+
+      if(random < 0.5)
+        this.currentStatus = 'online';
+      else if(random < 0.9)
+        this.currentStatus = 'offline';
+      else
+        this.currentStatus = 'unknown';
+    }, 5000);
+  }
 }
