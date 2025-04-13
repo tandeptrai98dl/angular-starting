49e5d8c20d9532b096f4895129f784c536ad7ef3
Tan Nguyen <tandeptrai98dl@gmail.com>
2025-04-13 15:28:40 +0700

    Done 147

---
+++ 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, model, Output, OutputEmitterRef } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+  // @Input({required: true}) size!:{width: string, height: string};
+  // @Output() sizeChange = new EventEmitter<{width: string, height: string}>();
+  size = model.required<{width: string, height: string}>();
+
+  onReset() {
+    this.size.set({
+      width: '200',
+      height: '100'
+    });
+  }
+}
