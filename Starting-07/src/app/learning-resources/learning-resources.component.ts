d1789636be50f38e33cfdf732ac418c963df993e
Tan Nguyen <42869550+tandeptrai98dl@users.noreply.github.com>
2026-05-15 23:49:13 +0700

    Done 157

--- Starting-07/src/app/learning-resources/learning-resources.component.ts
+++ Starting-07/src/app/learning-resources/learning-resources.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SafelinkDirective } from '../safe-link.directive';
 
 @Component({
   selector: 'app-learning-resources',
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css',
   standalone: true,
+  imports: [SafelinkDirective]
 })
-export class LearningResourcesComponent {}
+
+export class LearningResourcesComponent { }
--- Starting-07/src/app/safe-link.directive.ts
+++ Starting-07/src/app/safe-link.directive.ts
@@ -1,9 +1,27 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
 
 @Directive({
-    selector: '[app-safe-link]'
+    selector: 'a [appSafeLink]',
+    standalone: true,
+    host: {
+        '(click)': 'onClick($event)'
+    }
 })
+
 export class SafelinkDirective {
+    queryParams = input('appSafeLink', { alias: 'appSafeLink' });
 
+    constructor() {
+        console.log('SafeLinkDirective initialized');
+    }
 
+    onClick(event: Event) {
+        const wantsToNavigate = confirm('Are you sure you want to navigate to this link?');
+        if (!wantsToNavigate) {
+            event.preventDefault();
+        }
+
+        const address = (event.target as HTMLAnchorElement).href;
+        (event.target as HTMLAnchorElement).href = address + "?from=" + this.queryParams();
+    }
 }
